59b22ba98efd057b0e933ca4c78c1aa103bee3bd
Balata <JBalata@geico.com>
2017-03-17 15:50:23 -0400

    bootstrapping forms and navbar

--- src/components/blogForm/BlogEntryForm.js
+++ src/components/blogForm/BlogEntryForm.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Input from '../../common/textInput.js';
+import React from 'react'
+import Input from '../../common/textInput.js'
 
 const BlogEntryForm = (props) => {
     // ToDo: make these look good
--- src/components/sideBar/SideBarContainer.js
+++ src/components/sideBar/SideBarContainer.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router'
+import logo from '../../logo.svg'
+import '../../App.css'
+import { Navbar, NavItem, Nav } from 'react-bootstrap'
 
 class SideBar extends React.Component{
     render() {
         // ToDo: implement proper css for this
         return (
-            <div >
-                SideBar content will go here.
-                <li><Link to="/Blog" activeStyle={{ color: 'red' }}>Blog</Link></li>
-                <li><Link to="/AboutMe" activeStyle={{ color: 'red' }}>About Me</Link></li>
-                <li><Link to="/Resume" activeStyle={{ color: 'red' }}>Resume</Link></li>
+            <div>
+                <Navbar inverse collapseOnSelect>
+                    <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to="/Blog">Reactjs</Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                    <Nav pullRight>
+                        <NavItem><Link to="/Blog">Blog</Link></NavItem>
+                        <NavItem><Link to="/AboutMe">About Me</Link></NavItem>
+                        <NavItem><Link to="/Resume">Resume</Link></NavItem>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <img src={logo} className="App-logo" alt="logo" />
             </div>
         )
     }
